8679e2ddd638bcb25904fbcce2e91efcf7841087
ThinhLe1208 <lebaothinh1208@gmail.com>
2023-03-19 16:34:20 +0700

    cập nhật api

---
+++ controllers/api.js
@@ -0,0 +1,19 @@
+const getProducts = async () => {
+    const promise = await axios({
+        url: 'https://shop.cyberlearn.vn/api/Product',
+        method: 'GET'
+    });
+
+    console.log('api products : ', promise.data.content);
+    return promise.data.content;
+};
+
+const getById = async (id) => {
+    const promise = await axios({
+        url: `https://shop.cyberlearn.vn/api/Product/getbyid?id=${id}`,
+        method: 'GET'
+    });
+
+    console.log('api id : ', promise.data.content);
+    return promise.data.content;
+};
